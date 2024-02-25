e5b4f1b23d9f508cddef2ab240a1c559640dcdf5
kcsharry <107466346+kcsharry@users.noreply.github.com>
2024-02-25 20:42:19 +0000

    ugh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 //import ToolTip from '../uiAceternity/Tooltip';
 
+import { GoogleGeminiEffect } from "@/components/ui/google-gemini-effect";
+import { useMotionValue } from "framer-motion";
+
+const x = useMotionValue(0)
+
 export default function Home() {
   return (
-    <h1 className="text-red-300 text-bold">hi</h1>
+    <><h1 className="text-red-300 text-bold">hi</h1><><GoogleGeminiEffect pathLengths={[ x ]} /></></>
   );
 }
